19e2f3922505c186176f9e51b8de003d9b293801
Dmytro Klymchuk <srk90512@gmail.com>
2025-11-23 19:13:42 +0100

    feat: include users router in main application

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import likes, posts
+from routers import likes, posts, users
 from core.config import settings
 from routers import auth
 
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(likes.router)
 app.include_router(posts.router)
